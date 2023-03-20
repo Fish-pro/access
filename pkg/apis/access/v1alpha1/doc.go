9e8a142701bb4679350371510a1ee1caba711227
Zechun Chen <zechun.chen@daocloud.io>
2023-03-20 17:03:59 +0800

    replace license for go file

--- pkg/apis/access/v1alpha1/doc.go
+++ pkg/apis/access/v1alpha1/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 The Mca Authors.
+Copyright 2023 The access Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
