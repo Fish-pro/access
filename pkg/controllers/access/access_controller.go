f30423d4bedc4cb4717c1a8e367994ccd46df9cf
Zechun Chen <zechun.chen@daocloud.io>
2023-03-22 21:38:05 +0800

    update access to namespaced

--- pkg/controllers/access/access_controller.go
+++ pkg/controllers/access/access_controller.go
@@ -19,7 +19,6 @@ package access
 import (
 	"context"
 	"fmt"
-	"k8s.io/client-go/util/retry"
 	"os"
 	"strings"
 	"time"
@@ -38,6 +37,7 @@ import (
 	corelisters "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/retry"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
 
